9b7b251e647a242727660122e4ffce97dcb146b3
taicancr7 <taicancr7@gmail.com>
2020-11-05 22:52:10 +0700

    update

---
+++ -SoftwareTesting_lab-main/Lab/Test Suites/Test Suite lab08.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite lab08</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fb7be0dc-5c37-4d92-b0cd-a8bc2b116909</testSuiteGuid>
+   <testCaseLink>
+      <guid>54703c52-f748-4e6f-9c2c-2e2681d078e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lab8/Test01</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>921e48a4-f216-4bab-b3df-15e88d066938</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data1</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>925a9d64-0886-4c65-92d9-aed5e9525162</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>921e48a4-f216-4bab-b3df-15e88d066938</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>c256e3a0-db31-468e-a2c2-b2882a383d70</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>921e48a4-f216-4bab-b3df-15e88d066938</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>d7356a44-a7a3-4ce4-9fbd-b52be8509c0d</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
